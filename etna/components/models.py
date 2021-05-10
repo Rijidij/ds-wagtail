6067892bc5efc932c9e5d760d4ecc30d943e87e6
Craig Wood <craigwood@numiko.com>
2021-05-10 17:12:33 +0100

    Add multiple, orderable, selectable alerts

--- etna/components/models.py
+++ etna/components/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.admin.edit_handlers import StreamFieldPanel, InlinePanel
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+from modelcluster.fields import ParentalKey
 
 from .blocks import ComponentsStreamBlock
 
@@ -31,5 +33,22 @@ class ComponentsPage(Page):
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('content'),
-        SnippetChooserPanel('body_region_alert')
+        SnippetChooserPanel('body_region_alert'),
+        InlinePanel('alert_placements', label="Alerts"),
     ]
+
+
+class ComponentsPageAlertsPlacement(Orderable, models.Model):
+    page = ParentalKey('components.ComponentsPage', on_delete=models.CASCADE, related_name='alert_placements')
+    alert = models.ForeignKey('alerts.Alert', on_delete=models.CASCADE, related_name='+')
+
+    class Meta(Orderable.Meta):
+        verbose_name = "alert placement"
+        verbose_name_plural = "alert placements"
+
+    panels = [
+        SnippetChooserPanel('alert'),
+    ]
+
+    def __str__(self):
+        return self.page.title + " -> " + self.alert.title
