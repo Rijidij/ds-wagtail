899d8f5544ed59526b0e9504b81d80f62b27577b
Craig Wood <craigwood@numiko.com>
2021-05-10 14:08:19 +0100

    Active alerts template tag

--- etna/alerts/templatetags/alert_tags.py
+++ etna/alerts/templatetags/alert_tags.py
@@ -7,7 +7,9 @@ register = template.Library()
 @register.inclusion_tag('alerts/tags/alerts.html', takes_context=True)
 def alerts(context):
     """
-    Return all active alerts.
+    Return all active alert snippets.
+    Using this tag in the header template would be one way of displaying global alerts.
+    If this method is used, an additional field would be required to filter non-global (section/page) alerts.
     """
     return {
         'alerts': Alert.objects.filter(active=True),
