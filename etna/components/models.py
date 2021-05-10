b5a2cae32959008f015ddef29d1180b477297850
Craig Wood <craigwood@numiko.com>
2021-05-10 16:30:49 +0100

    Add body_region_alert field to components page

--- etna/components/models.py
+++ etna/components/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 from .blocks import ComponentsStreamBlock
 
@@ -18,6 +19,17 @@ class ComponentsPage(Page):
         null = True,
     )
 
+    # Alert to be displayed in the body region of the page.
+    # Additional fields could be added for the header and footer regions.
+    body_region_alert = models.ForeignKey(
+        'alerts.Alert',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     content_panels = Page.content_panels + [
         StreamFieldPanel('content'),
+        SnippetChooserPanel('body_region_alert')
     ]
