cca817e1e9f4fb376604a919d76f77cdf6fa84ca
Craig Wood <craigwood@numiko.com>
2021-05-11 17:20:07 +0100

    Allow alert snippets to "cascade" - appear on child pages

--- etna/alerts/templatetags/alert_tags.py
+++ etna/alerts/templatetags/alert_tags.py
@@ -1,9 +1,55 @@
 from django import template
+
+from wagtail.core.models import Page
+
 from ..models import Alert
+
+# Alert snippets
 
 register = template.Library()
 
-# Alert snippets
+
+def get_body_region_alert(page):
+    """
+    Get the body_region_alert field, if it exists.
+    """
+    alert = None
+    try:
+        alert = page.body_region_alert
+    except AttributeError:
+        pass
+
+    return alert
+
+
+@register.inclusion_tag('alerts/tags/alerts.html', takes_context=True)
+def alerts(context):
+    """
+    Return alerts from current page and those cascaded from ancestors.
+    To begin with, I'm just trying the single "body_region_alert" field.
+    """
+    page = context['page']
+    alerts = []
+
+    # Get alert for current page, if any.
+    alert = get_body_region_alert(page)
+    if alert:
+        alerts.insert(0, alert)
+
+    # Get current page ancestors.
+    branch = Page.objects.ancestor_of(page).specific()
+    # Iterate backwards and if page has alert and "cascade" is true,
+    # add to alerts list.
+    for i in reversed(range(branch.count())):
+        alert = get_body_region_alert(branch[i])
+        if alert and alert.cascade:
+            alerts.insert(0, alert)
+
+    return {
+        'alerts': alerts,
+        'request': context['request'],
+    }
+
 
 @register.inclusion_tag('alerts/tags/all_alerts.html', takes_context=True)
 def all_alerts(context):
