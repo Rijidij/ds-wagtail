3d2b02acbf69b8ee3d895c196627fad0bf11acbe
Craig Wood <craigwood@numiko.com>
2021-05-13 12:59:01 +0100

    Add jumplink Quote and Paragraph "wrapper" blocks

--- etna/components/blocks.py
+++ etna/components/blocks.py
@@ -2,8 +2,12 @@ from wagtail.core import blocks
 
 from ..text_formats.blocks import BasicRichTextBlock
 from ..quotes.blocks import QuoteBlock
+from ..jumplinks import blocks as JumplinkBlocks
 
 
 class ComponentsStreamBlock(blocks.StreamBlock):
+    jumplink = JumplinkBlocks.JumplinkBlock()
     paragraph = blocks.RichTextBlock()
+    jumplink_paragraph = JumplinkBlocks.JumplinkParagraphBlock()
     quote = QuoteBlock()
+    jumplink_quote = JumplinkBlocks.JumplinkQuoteBlock()
--- etna/jumplinks/blocks.py
+++ etna/jumplinks/blocks.py
@@ -1,5 +1,8 @@
 from wagtail.core import blocks
 
+from ..quotes.blocks import QuoteBlock
+
+
 class JumplinkBlock(blocks.StructBlock):
     label = blocks.CharBlock(required=False, max_length=80)
     anchor = blocks.CharBlock(required=False, max_length=80)
@@ -7,4 +10,26 @@ class JumplinkBlock(blocks.StructBlock):
     class Meta:
         icon = 'arrows-up-down'
         label = 'Jumplink'
+        template = 'jumplinks/blocks/jumplink.html'
+
+
+class JumplinkQuoteBlock(blocks.StructBlock):
+    label = blocks.CharBlock(required=False, max_length=80, label='Jumplink label')
+    anchor = blocks.CharBlock(required=False, max_length=80, label='Jumplink anchor')
+    block = QuoteBlock()
+
+    class Meta:
+        icon = 'openquote'
+        label = 'Jumplink Quote'
+        template = 'jumplinks/blocks/jumplink.html'
+
+
+class JumplinkParagraphBlock(blocks.StructBlock):
+    label = blocks.CharBlock(required=False, max_length=80, label='Jumplink label')
+    anchor = blocks.CharBlock(required=False, max_length=80, label='Jumplink anchor')
+    block = blocks.RichTextBlock(label='Paragraph')
+
+    class Meta:
+        icon = 'paragraph'
+        label = 'Jumplink Paragraph'
         template = 'jumplinks/blocks/jumplink.html'
