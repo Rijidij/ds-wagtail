e6bc3c2f5c0e45169a679b8c8f279704ce80da43
Craig Wood <craigwood@numiko.com>
2021-06-11 16:41:05 +0100

    YouTube and SoundCloud specific blocks no longer required

--- etna/streams/blocks.py
+++ etna/streams/blocks.py
@@ -15,29 +15,3 @@ class StreamEmbedBlock(blocks.StructBlock):
         template = "streams/blocks/stream-embed-block.html"
         icon = "media"
         label = "Streaming media player"
-
-
-class YouTubeBlock(blocks.StructBlock):
-    """ Embed a YouTube video player."""
-
-    share = EmbedBlock(label="YouTube share link")
-    title = blocks.TextBlock(required=False, max_length=100)
-    description = BasicRichTextBlock(required=False)
-
-    class Meta:
-        template = "streams/blocks/stream-embed-block.html"
-        icon = "media"
-        label = "YouTube video"
-
-
-class SoundCloudBlock(blocks.StructBlock):
-    """ Embed a SoundCloud audio player. """
-
-    share = EmbedBlock(label="SoundCloud share link")
-    title = blocks.TextBlock(required=False, max_length=100)
-    description = BasicRichTextBlock(required=False)
-
-    class Meta:
-        template = "streams/blocks/stream-embed-block.html"
-        icon = "media"
-        label = "SoundCloud audio"
