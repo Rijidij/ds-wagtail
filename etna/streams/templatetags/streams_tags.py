54c5f38909328250ecbf031bd964b625df322616
Craig Wood <craigwood@numiko.com>
2021-06-15 10:37:24 +0100

    Modify streamembed template tag to take context
    This avoids the need for multiple parameters for additional fields

--- etna/streams/templatetags/streams_tags.py
+++ etna/streams/templatetags/streams_tags.py
@@ -10,9 +10,10 @@ from wagtail.embeds.exceptions import EmbedException
 register = template.Library()
 
 
-@register.inclusion_tag(filename='streams/tags/streamembed.html', name='streamembed')
-def embed_tag(url, title=None, description=None):
+@register.inclusion_tag(filename='streams/tags/streamembed.html', takes_context=True, name='streamembed')
+def embed_tag(context):
     try:
+        url = context['self']['share'].url
         embed = embeds.get_embed(url)
         soup = BeautifulSoup(embed.html, 'html.parser')
 
@@ -22,9 +23,9 @@ def embed_tag(url, title=None, description=None):
             # the src attribute of the iframe
             'embediframesrc': soup.iframe['src'],
             # optional title
-            'title': title,
+            'title': context['self']['title'],
             # optional description
-            'description': description,
+            'description': context['self']['description'],
         }
     except EmbedException:
         return None
